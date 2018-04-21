0fde85b656f8f3145b53c53b502f6da7f6ccb737
Jesus Arnoldo Zerecero Nunez <arnoldoz97@hotmail.com>
2018-04-20 20:37:51 -0500

    Update main.c

--- TransformadaDiscretaFourier/Sources/main.c
+++ TransformadaDiscretaFourier/Sources/main.c
@@ -10,41 +10,41 @@
 
 ADCConfigInit_t InitAdcParameters = /*Inicializamos los valores de la estructura*/
 {
-		0, /*Se utilizar· bus clk*/ 
+		0, /*Se utilizar√° bus clk*/ 
 		0, /*Tiempo de muestreo corto*/
-		0, /*ResoluciÛn de 8 bits*/
-		0, /*Este par·metro se desprecia debido a que ser·n muestreos cortos*/
-		1, /*ConversiÛn de alta velocidad*/
-		0, /*Este par·metro se desprecia debido a que no se promedian muestras por hardware*/
+		0, /*Resoluci√≥n de 8 bits*/
+		0, /*Este par√°metro se desprecia debido a que ser√°n muestreos cortos*/
+		1, /*Conversi√≥n de alta velocidad*/
+		0, /*Este par√°metro se desprecia debido a que no se promedian muestras por hardware*/
 		0, /*Promediado por hardware desactivado*/
-		0, /*Este par·metro se desprecia debido a que no se promedian muestras por hardware*/
+		0, /*Este par√°metro se desprecia debido a que no se promedian muestras por hardware*/
 		OFFSET, /*Offset*/
 };
 
 int main(void)
 {
-	unsigned char baSenalEntrada[SIZE] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	unsigned char baSenalEntrada[SIZE] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; /*Pulso cuadrado*/
 	unsigned char *pbSenalEntrada = &baSenalEntrada[0];
-	signed long dbaDFTResult[SIZE]; /*Arreglo donde se almacenar· en main el resultado de la DFT*/
+	signed long dbaDFTResult[SIZE]; /*Arreglo donde se almacenar√° en main el resultado de la DFT*/
 	signed long *pdbDFTResult = &dbaDFTResult[0];
-	signed long *pdbReturnValue = 0; /*Apuntador para almacenar la direcciÛn del arreglo de retorno*/
+	signed long *pdbReturnValue = 0; /*Apuntador para almacenar la direcci√≥n del arreglo de retorno*/
 	unsigned short wCounter = SIZE; /*Contador para llenar los buffers*/
 	
-	vfnADCInit(&InitAdcParameters); /*InicializaciÛn del ADC*/
-	vfnPitInit(TARGET_TIME_100US,0); /*InicializaciÛn del PIT, sin interrupciones*/
+	vfnADCInit(&InitAdcParameters); /*Inicializaci√≥n del ADC*/
+	vfnPitInit(TARGET_TIME_100US,0); /*Inicializaci√≥n del PIT, sin interrupciones*/
 	vfnPitStart(); /*Comienza el contador del PIT*/
 	for(;;) 
 	{	
-	//	while(wCounter--) /*Se almacenan 128 elementos*/
-		//{
-			//while(PIT_CVAL0) /*Se espera a que termine la cuenta del PIT (100us)*/
-			//{
+		while(wCounter--) /*Se almacenan 128 elementos*/
+		{
+			while(PIT_CVAL0) /*Se espera a que termine la cuenta del PIT (100us)*/
+			{
 				/*Poll*/
-			//}
-			//*(pbSenalEntrada++) = wfnADCLectura(0); /*Se almacena lo que se leyÛ del ADC*/
-		//}
-		//pbSenalEntrada = &baSenalEntrada[0];
-		//wCounter = SIZE;
+			}
+			*(pbSenalEntrada++) = wfnADCLectura(0); /*Se almacena lo que se ley√≥ del ADC*/
+		}
+		pbSenalEntrada = &baSenalEntrada[0];
+		wCounter = SIZE;
 
 		pdbReturnValue = dbfnDFT(&baSenalEntrada[0], SIZE);
 				
